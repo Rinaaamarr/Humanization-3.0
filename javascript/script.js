db7dd6440085d6c6de450d1d8e374541238e5704
Rinaaamarr <marinova.arinaa@gmail.com>
2025-06-27 08:56:13 +0300

    404-finish

--- javascript/script.js
+++ javascript/script.js
@@ -2,10 +2,9 @@ console.log('check')
 document.addEventListener('DOMContentLoaded', () => {
   parallax()
   glitch()
+  glitch2()
   flipCard1()
   flipCard2()
-  myFunction()
-  //   windowOpen()
   //   refresh()
 })
 
@@ -27,13 +26,49 @@ function parallax() {
 }
 
 function glitch() {
-  const chars = ' @#%!^ @%#^&! *&^% '
-  const glitchDuration = 1000
+  const chars = ' @#%!^&*'
+  const glitchDuration = 3000
   const interval = 30
 
-  const glitchElements = document.querySelectorAll(
-    '.word1, .word2, .word3, .word4, .word5 '
-  )
+  const glitchElements = document.querySelectorAll('.word1')
+
+  glitchElements.forEach((el) => {
+    const originalText = el.dataset.text || el.textContent
+
+    el.addEventListener('mouseenter', () => {
+      let frame = 0
+      const length = originalText.length
+
+      const randomChar = () => chars[Math.floor(Math.random() * chars.length)]
+
+      const glitchInterval = setInterval(() => {
+        let output = ''
+
+        for (let i = 0; i < length; i++) {
+          if (i < frame) {
+            output += originalText[i]
+          } else {
+            output += randomChar()
+          }
+        }
+        el.textContent = output
+
+        frame++
+
+        if (frame > length) {
+          clearInterval(glitchInterval)
+          el.textContent = originalText
+        }
+      }, interval)
+    })
+  })
+}
+function glitch2() {
+  const chars = ' @#%!^&*'
+  const glitchDuration = 3000
+  const interval = 30
+
+  const glitchElements = document.querySelectorAll('.backword')
 
   glitchElements.forEach((el) => {
     const originalText = el.dataset.text || el.textContent
@@ -67,30 +102,6 @@ function glitch() {
   })
 }
 
-  var line1 = document.getElementById('line1')
-  var length = line1.getTotalLength()
-
-  // The start position of the drawing
-  line1.style.strokeDasharray = length
-
-  // Hide the triangle by offsetting dash. Remove this line to show the triangle before scroll draw
-  line1.style.strokeDashoffset = length
-
-  // Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
-  window.addEventListener('scroll', myFunction)
-
-  function myFunction() {
-    var scrollpercent =
-      (document.body.scrollTop + document.documentElement.scrollTop) /
-      (document.documentElement.scrollHeight -
-        document.documentElement.clientHeight)
-
-    var draw = length * scrollpercent
-
-    // Reverse the drawing (when scrolling upwards)
-    line1.style.strokeDashoffset = length - draw
-  }
-
 function flipCard1() {
   const card = document.querySelector('.card__inner')
 
@@ -105,26 +116,6 @@ function flipCard2() {
     card.classList.toggle('is-flipped')
   })
 }
-
-// function windowOpen() {
-//   const modal = document.querySelector('#modal')
-//   const btn = document.querySelector('#itemOne')
-//   const close = document.querySelector('.close')
-
-//   btn.onclick = function () {
-//     modal.style.visibility = '1'
-//   }
-
-//   close.onclick = function () {
-//     modal.style.display = 'none'
-//   }
-
-//   window.onclick = function (event) {
-//     if (event.target == modal) {
-//       modal.style.display = 'none'
-//     }
-//   }
-// }
 
 // function refresh() {
 //   const button = document.querySelector('.refresh')
