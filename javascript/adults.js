29eaa573a86eb4eed57ea1cc7edce5ed1e6388a7
Rinaaamarr <marinova.arinaa@gmail.com>
2025-06-27 03:53:44 +0300

    adults-finish

--- javascript/adults.js
+++ javascript/adults.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   flipCard3()
   flipCard4()
+  flipCard5()
+  flipCard6()
 })
 
 function flipCard3() {
@@ -12,6 +14,20 @@ function flipCard3() {
 }
 function flipCard4() {
   const card = document.querySelector('.card__inner-2')
+
+  card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped')
+  })
+}
+function flipCard5() {
+  const card = document.querySelector('.card__inner3')
+
+  card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped')
+  })
+}
+function flipCard6() {
+  const card = document.querySelector('.card__inner4')
 
   card.addEventListener('click', function () {
     card.classList.toggle('is-flipped')
